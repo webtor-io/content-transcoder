564eb16c528c3ff3a9b07cbc95f01b3c87a41477
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2020-11-03 19:58:59 +0300

    change settings

--- server/hls.go
+++ server/hls.go
@@ -336,10 +336,10 @@ func genFFmpegParams(in string, out string, options *Options, pr *cp.ProbeReply,
 		if options.forceTranscode || videoStrm.GetCodecName() != options.videoCodec || err == nil && bitrate > 2_000_000 {
 			params = append(params, options.videoCodec)
 			// https://gist.github.com/kuntau/a7cbe28df82380fd3467#gistcomment-2388045
-			params = append(params, "-crf", "27", "-x264-params", "cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-inter=21:deadzone-intra=11:b-adapt=2:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:qpmax=69:bframes=5:b-adapt=2:direct=auto:crf-max=51:weightp=2:merange=24:chroma-qp-offset=-1:sync-lookahead=2:psy-rd=1.00,0.15:trellis=2:min-keyint=23:partitions=all")
-			// params = append(params, "-b:v", "2M")
-			// params = append(params, "-maxrate", "2M")
-			// params = append(params, "-bufsize", "1M")
+			// params = append(params, "-crf", "27", "-x264-params", "cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-inter=21:deadzone-intra=11:b-adapt=2:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:qpmax=69:bframes=5:b-adapt=2:direct=auto:crf-max=51:weightp=2:merange=24:chroma-qp-offset=-1:sync-lookahead=2:psy-rd=1.00,0.15:trellis=2:min-keyint=23:partitions=all")
+			params = append(params, "-b:v", "2M")
+			params = append(params, "-maxrate", "2M")
+			params = append(params, "-bufsize", "1M")
 			// params = append(params, "-x264-params", "keyint=240:scenecut=0")
 			// params = append(params, "-r", "24")
 		} else {
